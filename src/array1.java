bca7818c2b56fbebc9356d1f2f2a298d6189687d
SACHITHA <thulshansachitha99@gamail.com>
2023-10-12 17:55:52 +0530

    java question answer for in class

--- src/array1.java
+++ src/array1.java
@@ -6,17 +6,17 @@ public class array1 {
         Scanner scanner = new Scanner(System.in);
         double[] student = new double[5];
         double max = 0;
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Enter ");
-            double height = scanner.nextDouble();
 
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter Height: ");
+            double height = scanner.nextDouble();
             student[i] = height;
         }
+
         for (int j = 0; j < student.length; j++) {
             if (max < student[j])
                 max = student[j];
         }
-
-        System.out.println(max);
+        System.out.println("Max Highest Student Hight " + max);
     }
 }
