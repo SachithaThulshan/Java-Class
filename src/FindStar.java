9017393223cdf9cac4417bcf44bc0bbb5479d643
Sachitha Thulshan <110708675+SachithaThulshan@users.noreply.github.com>
2023-10-15 22:37:03 +0530

    FindStar.java

--- src/FindStar.java
+++ src/FindStar.java
@@ -4,21 +4,21 @@ public class FindStar {
     public static void main(String[] args) {
 
         int a= 25;
-        int[][] star = new int[a][a];
-        String[][] outStar = new String[a][a];
-        Random random = new Random();
+        int[][] star = new int[a][a];              // Filling the numbers
+        String[][] outStar = new String[a][a];    // Filling the only "*" & " "
+        Random random = new Random();             
 
-        for (int i = 0; i < a; i++) {
+        for (int i = 0; i < a; i++) {                // adding randomly numbers for block 
             for (int j = 0; j < a; j++) {
-                star[i][j] = random.nextInt(11);
+                star[i][j] = random.nextInt(11);    // Random numbers generate(0 - 10)
             }
         }
 
         for (int i = 1; i < a-1; i+=1) {
             for (int j = 1; j < a-1; j+=1) {
 
-
 //                int current_position = star[i][j];
+                
 //                int left_cell = star[i][j - 1];
 //                int left_top_cell = star[i - 1][j - 1];
 //                int left_bottom_cell = star[i - 1][j];
@@ -30,15 +30,14 @@ public class FindStar {
 //                int up_cell = star[i - 1][j];
 //                int bottom_cell = star[i + 1][j];
 
-
                 if (star[i][j] > star[i][j - 1] && star[i][j] > star[i - 1][j - 1] && star[i][j] > star[i - 1][j] && star[i][j] > star[i][j + 1]
                         && star[i][j] > star[i - 1][j + 1] && star[i][j] > star[i + 1][j + 1] && star[i][j] > star[i - 1][j] && star[i][j] > star[i + 1][j])
-                    outStar[i][j] = "* ";
+                    outStar[i][j] = "* ";            //check center postion and surroud cell 8
                 else
                     outStar[i][j] = "  ";
             }
         }
-        for (int i = 1; i < 24; i++) {
+        for (int i = 1; i < 24; i++) {            //out "*" & " " array 
             for (int j = 1; j < 24; j++) {
                 System.out.print(outStar[i][j]);
             }
