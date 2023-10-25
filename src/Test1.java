ff5769352b60f08fcc731db6b9bf49bf8bcb9f13
SACHITHA <thulshansachitha99@gamail.com>
2023-10-26 00:48:00 +0530

    java question answer for in class

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,19 @@
 public class Test1 {
     public static void main(String[] args) {
+        CalculateAge dob = new CalculateAge();
+        dob.setToday("2023","10","26");
+        System.out.println("Your Age Year is: " + dob.calYear());
+        System.out.println("Your Age Month is: " + dob.calMonth());
+        System.out.println("Your Age Day is: " + dob.calDay());
+
+        System.out.println();
+
         FutureDate futureDate = new FutureDate();
         futureDate.setdate("2023","10","25");
         System.out.println(futureDate.addYear(10));
         System.out.println(futureDate.addMonth(2));
         System.out.println(futureDate.addDay(659));
-        System.out.println(futureDate.getDate());
+//        System.out.println(futureDate.getDate());
     }
 }
 
